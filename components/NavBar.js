4644329b8862fa3d4edf90b97c0abac35cb22b30
tor <torfrancis447@gmail.com>
2023-03-27 20:36:20 -0400

    add react icons

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,21 +1,40 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 const NavBar = () => {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
-        <div ClassName='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+        
             <Image src="/../public/assets/Tor-logo.png" alt="/" width='125' height='50'/>
             <div>
-                <ul>
+                <ul className='hidden md:flex'>
                     <Link href='/'>
-                        <li> Home </li>
+                        <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+                    </Link>
+                    <Link href='/'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'> About </li>
+                    </Link>
+                    <Link href='/'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'> Skills </li>
+                    </Link>
+                    <Link href='/'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'> Projects </li>
+                    </Link>
+                    <Link href='/'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'> Contact </li>
                     </Link>
                 </ul>
+                <div className="md:hidden pr-2">
+                    <AiOutlineMenu size={25} />
+                </div>
             </div>
         </div>
-        
+
+        <div className='fixed left-0 w-full'>
+            <h1> Hello from side menu </h1>
+        </div>      
     </div>
   )
 }
