c8bbe3068dc3b993e41f60b8522004916884e181
tor <torfrancis447@gmail.com>
2023-04-03 14:08:20 -0400

    added google maps component

--- components/Map.js
+++ components/Map.js
@@ -1,13 +1,24 @@
 import GoogleMapReact from 'google-map-react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+
 
 const Map = ({ center, zoom }) => {
+  // const = {} = useLoadScript({
+  //   googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  // })
+
   return (
-    <div style={{ height: '500px', width: '100%' }}>
+    <div style={{ height: '500px', width: '80%' }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyBLE_tAiDBNd4Ug55l1xYU3AH-48U2Ogy8' }}
+        bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY }}
         defaultCenter={center}
         defaultZoom={zoom}
       >
+      {/* <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text="My Marker"
+        /> */}
       </GoogleMapReact>
     </div>
   );
