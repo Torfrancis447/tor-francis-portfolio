a8e3674b86c0ff36103f27b24f2335f8605c2af4
tor <torfrancis447@gmail.com>
2023-04-04 16:30:39 -0400

    build error 2

--- components/NavBar.js
+++ components/NavBar.js
@@ -129,7 +129,7 @@ const NavBar = () => {
             </ul>
             <div className="pt-40">
               <p className="uppercase tracking-widest text-[#5651e5]">
-                Let's Connect
+                {`Let's Connect`}
               </p>
               <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
                 <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
