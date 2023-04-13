4107320cfc61b94c7c4486de1ab8088b0ee989ac
tor <torfrancis447@gmail.com>
2023-04-13 13:49:36 -0400

    fix config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN,
+  }
 }
 
 module.exports = nextConfig
